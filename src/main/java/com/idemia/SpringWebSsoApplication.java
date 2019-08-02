2b54fdd7d72391357b54f2e495ca0e9d70e512f1
Narender <Narender.Majoka@idemia.com>
2019-08-02 15:54:04 +0530

    working project uploaded

---
+++ src/main/java/com/idemia/SpringWebSsoApplication.java
@@ -0,0 +1,13 @@
+package com.idemia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSsoApplication.class, args);
+	}
+
+}
